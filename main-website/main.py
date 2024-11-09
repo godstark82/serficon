8878d899325886667aba119c00ffcc944d29bb7e
VENOM LEADER <79461207+godstark82@users.noreply.github.com>
2024-11-09 22:45:31 +0530

    added some env variables

--- main-website/main.py
+++ main-website/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, flash, redirect, url_for
 from db_instance import get_db
 from models.committee_model import CommitteeRole
@@ -5,61 +6,64 @@ from services import committee_service, faq_service, home_service, imp_dates_ser
 
 app = Flask(__name__)
 app.secret_key = 'conference_2024secretkey'
+website_title = os.getenv('website_title')
+navbar_title = os.getenv('navbar_title')
+domain = os.getenv('domain')
 
 
 @app.route("/")
 def Home():
     home = home_service.get_home_data()
-    return render_template('index.html', home=home)
+    return render_template('index.html', home=home, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/organizing-committee")
 def OrganizingCommittee():
     title = "Organizing Committee"
     members = committee_service.get_all_committee_members()
     members = [member for member in members if member.role == CommitteeRole.ORGANISING_COMMITTEE.value]
-    return render_template('screens/about/oc.html', title=title, members=members)
+    return render_template('screens/about/oc.html', title=title, members=members, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/scientific-committee")
 def ScientificCommittee():
     title = "Scientific Committee"
     members = committee_service.get_all_committee_members()
     members = [member for member in members if member.role == CommitteeRole.SCIENTIFIC_COMMITTEE_MEMBER.value]
-    return render_template('screens/about/oc.html', title=title, members=members)
+    return render_template('screens/about/oc.html', title=title, members=members, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 
 
 @app.route("/registration")
 def Registration():
-    return render_template('registration.html')
+    return render_template('registration.html', website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/important-dates")
 def ImportantDates():
     dates = imp_dates_service.get_imp_dates()
-    return render_template('screens/Guide for authors/importantdates.html', dates=dates)
+    return render_template('screens/Guide for authors/importantdates.html', dates=dates, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/review-process")
 def ReviewProcess():
     review_process = faq_service.get_review_process()
-    return render_template('screens/Guide for authors/reviewprocess.html', review_process=review_process)
+    return render_template('screens/Guide for authors/reviewprocess.html', review_process=review_process, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/submission-guidelines")
 def SubmissionGuidelines():
     submission_guidelines = faq_service.get_submission_guidelines()
-    return render_template('screens/Guide for authors/subgl.html', guidelines=submission_guidelines)
+    return render_template('screens/Guide for authors/subgl.html', guidelines=submission_guidelines, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/paper-status")
 def PaperStatus():
-    return render_template('screens/Guide for authors/paperstatus.html')
+    return render_template('screens/Guide for authors/paperstatus.html', website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/detailed-schedule")
 def DetailedSchedule():
     schedules = schedule_service.get_schedule()
-    return render_template('screens/program/detailedschedule.html', schedules=schedules)
+    return render_template('screens/program/detailedschedule.html', schedules=schedules, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/awards-grants")
 def AwardsAndGrants():
     rewards = faq_service.get_rewards_grants()
-    return render_template('screens/program/awardsngrants.html', rewards=rewards)
+    return render_template('screens/program/awardsngrants.html', rewards=rewards, website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 @app.route("/contact", methods=['GET', 'POST'])
 def Contact():
@@ -78,7 +82,7 @@ def Contact():
         flash("Message sent successfully", "success")
         return redirect(url_for('Contact')) 
 
-    return render_template('contact.html')
+    return render_template('contact.html', website_title=website_title, navbar_title=navbar_title, domain=domain)
 
 # @app.route("/social-links")
 # def SocialLinks():

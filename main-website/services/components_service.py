0d2bec4d19d4d6a36e9a41ed494cabf4f1ac39b0
VENOM LEADER <79461207+godstark82@users.noreply.github.com>
2024-12-09 23:48:28 +0530

    order fixed in navbar #2

--- main-website/services/components_service.py
+++ main-website/services/components_service.py
@@ -5,6 +5,10 @@ from models.components_model import ComponentsModel, NavItem
 def get_navbar_items()-> List[NavItem]:
     doc = db.collection('navigation').get()
     nav_items = [NavItem.from_dict(data=item.to_dict()) for item in doc]
+    
+    for item in nav_items:
+        item.children = sorted(item.children, key=lambda x: x.order)
+    
     return sorted(nav_items, key=lambda x: x.order)
 
 def get_all_components()-> ComponentsModel:

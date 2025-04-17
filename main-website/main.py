b51d5964ab0241aadbee04b2048a4b140bad4e80
Lalit Sharma <ls8290519977@gmail.com>
2025-04-17 18:28:19 +0530

    improved SEO added sitemap and robots txt

--- main-website/main.py
+++ main-website/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from services import components_service, page_service, imp_dates_service, schedule_service, reward_service, home_service
 from routes.registration import registration_bp
 from routes.contact import contact_bp
 from routes.home import webcomponents_bp
-
+from models.components_model import ComponentsModel
 
 app = Flask(__name__)
 app.secret_key = 'conference_2024secretkey'
@@ -20,6 +20,61 @@ def page_not_found(e):
     navItems = components_service.get_navbar_items()    
     components = components_service.get_all_components()
     return render_template('pages/404.html', components=components, sections=sections, navItems=navItems)
+
+
+# static files
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(app.static_folder, filename)
+
+# robots.txt
+@app.route('/robots.txt')
+def robots_txt():
+    components: ComponentsModel = components_service.get_all_components()
+    robots_content = f"""User-agent: *
+Allow: /
+Disallow: /admin/   
+Sitemap: https://www.{components.domain}/sitemap.xml
+"""
+    return robots_content, 200, {'Content-Type': 'text/plain'}
+
+# sitemap.xml
+@app.route('/sitemap.xml')
+def sitemap_xml():
+    components: ComponentsModel = components_service.get_all_components()
+    navItems = components_service.get_navbar_items()
+    
+    # Start the sitemap XML
+    sitemap_content = f"""<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://www.{components.domain}/</loc>
+        <priority>1.0</priority>
+    </url>
+"""
+    
+    # Add all navigation items to the sitemap
+    for nav_item in navItems:
+        if nav_item.path:
+            sitemap_content += f"""    <url>
+        <loc>https://www.{components.domain}{nav_item.path}</loc>
+        <priority>0.8</priority>
+    </url>
+"""
+        # Add child pages
+        for child in nav_item.children:
+            if child.path:
+                sitemap_content += f"""    <url>
+        <loc>https://www.{components.domain}{child.path}</loc>
+        <priority>0.7</priority>
+    </url>
+"""
+    
+    # Close the sitemap
+    sitemap_content += "</urlset>"
+    
+    return sitemap_content, 200, {'Content-Type': 'application/xml'}
+
 
 
 @app.route('/<path>/<subpath>/<id>')
